07090e499ca2b7726566df45efb15e3a2dc7a644
Stefan Teunissen <stefanteunissen1@gmail.com>
2018-02-14 23:08:50 +0100

    Init

---
+++ Skelet/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Skelet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linesLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(168)))), ((int)(((byte)(168)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.linesLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 534);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(847, 27);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(433, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ctrl+S=Save Ctrl+O=Open Ctrl+N=New Alt+Q=Quit";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // linesLabel
+            // 
+            this.linesLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linesLabel.AutoSize = true;
+            this.linesLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.linesLabel.Location = new System.Drawing.Point(3, 4);
+            this.linesLabel.Name = "linesLabel";
+            this.linesLabel.Size = new System.Drawing.Size(81, 19);
+            this.linesLabel.TabIndex = 1;
+            this.linesLabel.Text = "Lines: 1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(847, 534);
+            this.panel2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(168)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(168)))), ((int)(((byte)(168)))));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(847, 534);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 561);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Skelet";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label linesLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Skelet/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Skelet
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool CreateCaret(IntPtr hWnd, IntPtr hBmp, int w, int h);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetCaretPos(int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool ShowCaret(IntPtr hWnd);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool DestroyCaret();
+
+        string openedFile = "";
+        bool saved = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox1.GotFocus += OnFocus;
+            this.textBox1.LostFocus += OnDefocus;
+            this.GotFocus += FormGotFocus;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+        }
+
+        private void OnFocus(object sender, EventArgs e)
+        {
+            CreateCaret(textBox1.Handle, IntPtr.Zero, 8, Int32.Parse(((textBox1.Font.Size - 2)*2).ToString()));
+            SetCaretPos(2, 1);
+            ShowCaret(textBox1.Handle);
+            base.OnGotFocus(e);
+        }
+
+        private void OnDefocus(object sender, EventArgs e)
+        {
+            DestroyCaret();
+            base.OnLostFocus(e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void SaveFile()
+        {
+            if (openedFile == "")
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.AddExtension = true;
+                sfd.DefaultExt = ".txt";
+                sfd.Filter = "Text file(*.txt)|*.txt|Any(*.*)|*.*";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    openedFile = sfd.FileName;
+                    saven();
+                    saved = true;
+                }
+            }
+            else
+            {
+                saven();
+                saved = true;
+            }
+            SetTitle();
+        }
+
+        void saven()
+        {
+            using (StreamWriter sw = new StreamWriter(openedFile))
+            {
+                foreach (string line in textBox1.Lines)
+                {
+                    sw.WriteLine(line);
+                }
+            }
+        }
+        
+        void OpenFile()
+        {
+            if (saved || textBox1.Text == "")
+            {
+                openen();
+            }
+            else
+            {
+                DialogResult dr = MessageBox.Show("Your file has not been saved! Save?", "Unsaved", MessageBoxButtons.YesNoCancel);
+                if (dr == DialogResult.Yes)
+                {
+                    SaveFile();
+                    openen();
+                }
+                else if (dr == DialogResult.No)
+                {
+                    openen();
+                }
+            }
+        }
+
+        void openen()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.AddExtension = true;
+            ofd.Filter = "Text file(*.txt)|*.txt|Any(*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = "";
+                openedFile = ofd.FileName;
+                using (StreamReader sr = new StreamReader(openedFile))
+                {
+                    string line = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        textBox1.Text += line+Environment.NewLine;
+                    }
+                }
+                saved = true;
+            }
+            SetTitle();
+        }
+
+        void SetTitle()
+        {
+            string title = "Skelet | ";
+            if (openedFile != "")
+            {
+                title += openedFile;
+            }
+            if (!saved)
+            {
+                title += "*";
+            }
+            this.Text = title;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.S))
+            {
+                SaveFile();
+                return true;
+            }
+            else if (keyData == (Keys.Control | Keys.N))
+            {
+                var info = new System.Diagnostics.ProcessStartInfo(Application.ExecutablePath);
+                System.Diagnostics.Process.Start(info);
+                return true;
+            }
+            else if (keyData == (Keys.Control | Keys.O))
+            {
+                OpenFile();
+                return true;
+            }
+            else if (keyData == (Keys.Control | Keys.Back))
+            {
+                SendKeys.SendWait("^+{LEFT}{BACKSPACE}");
+                return true;
+            }
+            else if (keyData == (Keys.Tab))
+            {
+                textBox1.SelectedText = "    ";
+            }
+            else if (keyData == (Keys.Alt | Keys.Q))
+            {
+                Application.Exit();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        void FormGotFocus(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            saved = false;
+            linesLabel.Text = "Lines: "+textBox1.Lines.Length.ToString();
+            SetTitle();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                textBox1.SelectAll();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (saved || textBox1.Text == "")
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                DialogResult dr = MessageBox.Show("Your file has not been saved! Save?", "Unsaved", MessageBoxButtons.YesNoCancel);
+                if (dr == DialogResult.Yes)
+                {
+                    SaveFile();
+                    e.Cancel = false;
+                }
+                else if (dr == DialogResult.No)
+                {
+                    e.Cancel = false;
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
